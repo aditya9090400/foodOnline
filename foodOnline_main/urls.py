871c0b4d0b8a4a6f05c1d2b563566063a7731700
aditya-cns <aditya.pradhan@capitalnumbers.com>
2023-11-15 11:10:03 +0530

    links updated. Section 6 completed.

--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index, name="index"),
+    path("", views.index, name="home"),
 
     path("accounts/", include("accounts.urls")),
 
