4fc74fd60978f7e0872898b48d0334603e496c3b
aditya-cns <aditya.pradhan@capitalnumbers.com>
2023-11-24 00:22:18 +0530

    admin approval logic added

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,10 +1,9 @@
-from django.contrib import messages
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
-from django.core.mail import EmailMessage, message
+from django.core.mail import EmailMessage
 from django.conf import settings
 
 
@@ -31,3 +30,16 @@ def detectUser(user):
     elif user.role is None and user.is_superadmin:
         redirectUrl = '/admin'
     return redirectUrl
+
+
+def send_notification(mail_subject, mail_template, context):
+    from_email = settings.DEFAULT_FROM_EMAIL
+    message = render_to_string(mail_template, context)
+    if (isinstance(context['to_email'], str)):
+        to_email = []
+        to_email.append(context['to_email'])
+    else:
+        to_email = context['to_email']
+    mail = EmailMessage(mail_subject, message, from_email, to=to_email)
+    mail.content_subtype = "html"
+    mail.send()
