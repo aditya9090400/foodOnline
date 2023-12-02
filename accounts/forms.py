72a0f417eb7de4dd37885d3e78bd0e21e63599d0
aditya-cns <aditya.pradhan@capitalnumbers.com>
2023-12-02 09:25:29 +0530

    Section-10: Make restaurant profile form & custom validators

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,12 +2,13 @@ from django import forms
 from .models import User  # , UserProfile
 # from .validators import allow_only_images_validator
 from vendor.models import Vendor
-# from accounts.validators import allow_only_images_validator
+from .models import UserProfile
+from accounts.validators import allow_only_images_validator
 
 
 class VendorForm(forms.ModelForm):
     vendor_license = forms.FileField(widget=forms.FileInput(
-        attrs={'class': 'btn btn-info'}))
+        attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
 
     class Meta:
         model = Vendor
@@ -31,3 +32,25 @@ class UserForm(forms.ModelForm):
             raise forms.ValidationError(
                 "Password does not match!"
             )
+
+
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Start typing...', 'required': 'required'}))
+    profile_picture = forms.FileField(
+        widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(
+        widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+
+    # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address', 'country',
+                  'state', 'city', 'pin_code', 'latitude', 'longitude']
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
